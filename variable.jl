f8dd35915b92db0cb927bc420759b6c8ec141b61
hideonatc <stanley013060@gmail.com>
2020-05-24 16:49:36 +0800

    --all

---
+++ variable.jl
@@ -0,0 +1,14 @@
+x = 5
+println(typeof(x)) #Int64
+y = 1.0
+println(typeof(y)) #Float64
+z = 5::Int32
+println(typeof(z)) #Int32
+
+####
+
+x = 1+1 #x=2
+x = 2.0-1 #x=1.0
+x = 3.14*4 #x=12.56
+x = 3.3/3 #x=1.1
+x = 2^5 #x=32
